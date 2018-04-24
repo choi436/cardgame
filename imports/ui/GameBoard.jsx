4f9bdbc2aca6c65506554eb107327c6ee34fe072
Choi Jaewoong <choi436@purdue.edu>
2018-04-24 19:09:59 -0400

    game now recognizes a win condition

--- imports/ui/GameBoard.jsx
+++ imports/ui/GameBoard.jsx
@@ -2,16 +2,6 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 
 export default class GameBoard extends Component {
-
-  /*handleCellClick(row, col) {
-    let currentPlayer = this.currentPlayer();
-    let game = this.props.game;
-    game.board[row][col] = currentPlayer;
-    Games.update(game._id, {
-      $set: {board: game.board}
-    });
-  }*/
-
   checkWin(player) {
     var hand;
     if (player == 1) {
@@ -230,55 +220,6 @@ export default class GameBoard extends Component {
       }
     }
   }
-
-  /*renderCell(row, col) {
-    let value = this.props.game.board[row][col];
-    if (value === 0) return (<td>O</td>);
-    if (value === 1) return (<td>X</td>);
-    if (value === null) return (
-      <td onClick={this.handleCellClick.bind(this, row, col)}></td>
-    );
-  }*/
-
-  /*rcard(arr, col) {
-    let game = this.props.game;
-    var gotten;
-    var url;
-    if (arr === 0) {
-      if (game.twochose !== 0) {
-        return (<img src="/PNG/card_back.png" width="69" height="105">);
-      }
-      return (<img src="/PNG/0.png" width="69" height="105">);
-    } else if (arr === 1) {
-      if (game.twochose === 0) {
-        return (<img src="/PNG/0.png" width="69" height="105">);
-      }
-      gotten = game.ontable[col];
-    } else if (arr === 3) {
-      if (col === 0) {
-        url = "/PNG/" + game.onediscard + ".png";
-      } else {
-        url = "/PNG/" + game.twodiscard + ".png";
-      }
-      return (<img src=url width="69" height="105">);
-    } else {
-      if (game.twochose === 0) {
-        if (col !== 10) {
-          gotten = game.twohand[col];
-        } else {
-          gotten = 0;
-        }
-        url = "/PNG/" + gotten + ".png";
-        return (<img src=url width="69" height="105">);
-      } else if (col === 10) {
-        gotten = game.twochose;
-      } else {
-        gotten = game.twohand[col];
-      }
-    }
-    url = "/PNG/" + gotten + ".png";
-    return (<img src=url width="69" height="105" onClick={this.handleClick.bind(this,arr,col)}>);
-  }*/
 
   whochoose() {
     if (this.props.game.playerOne.username == this.props.user.username) {
